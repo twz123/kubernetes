ec2fe05b0ea799a341681bdc22311dc84f3527e8
Tom Wieczorek <twieczorek@mirantis.com>
2023-07-05 17:10:25 +0200

    Use /usr/bin/env shebang for better portability

--- staging/src/k8s.io/client-go/plugin/pkg/client/auth/exec/testdata/test-plugin.sh
+++ staging/src/k8s.io/client-go/plugin/pkg/client/auth/exec/testdata/test-plugin.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
 
 # Copyright 2018 The Kubernetes Authors.
 #
@@ -13,6 +13,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+set -e
 
 >&2 echo "$KUBERNETES_EXEC_INFO"
 echo "$TEST_OUTPUT"
